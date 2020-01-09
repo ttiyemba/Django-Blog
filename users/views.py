4c50cc5b5b01ea980013763396085c610eb5ce6a
ttiyemba <tapiwatiyemba@icloud.com>
2020-01-09 20:47:50 +0000

    fully functional register frontend

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms  import UserRegisterForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST) # if post request assign userform with that post data
+        if form.is_valid():
+            form.save() #save the user
+            username = form.cleaned_data.get('username') #if form is valid return the registered username
+            messages.success(request, f'Account successfully created for {username}!') #Flash message to display to user
+            return redirect('blog-home') # redirect to homepage after registeration
+    else:
+        form = UserRegisterForm() # if not post request assign   blank form
     return render(request, 'users/register.html', {'form': form})
